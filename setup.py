6e9b27e83a72c986b8a5133e73e0fbea9a59fdfb
Jeff Hann <jeffhann@gmail.com>
2016-11-19 16:33:00 -0400

    adding license and setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='SantaBot',
+      version='0.0.1',
+      description='Helping santa',
+      author='Jeff Hann',
+      author_email='jeffhann@gmail.com',
+      url='http://www.jeffreyhann.ca/',
+      )
